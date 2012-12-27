bda378572255e6634a76964d3d11e5332fad6709
troy <troy@webhelpdesk.info>
2012-12-26 17:45:58 -0700

    Removed some unused imports.

--- Sources/com/test/Application.java
+++ Sources/com/test/Application.java
@@ -1,19 +1,11 @@
 package com.test;
 
 import com.webobjects.appserver.WOApplication;
-import com.webobjects.appserver.WOMessage;
-import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.foundation.NSNotification;
 import com.webobjects.foundation.NSNotificationCenter;
 import com.webobjects.foundation.NSSelector;
-import com.webobjects.foundation.NSTimeZone;
-import com.webobjects.foundation._NSUtilities;
 
 import er.extensions.appserver.ERXApplication;
-import er.extensions.appserver.ERXMessageEncoding;
-import er.extensions.appserver.ERXResponseRewriter;
-import er.extensions.appserver.ERXResponseRewriter.Resource;
-import er.extensions.eof.ERXEC;
 import er.extensions.eof.ERXEntityClassDescription;
 import er.extensions.eof.ERXObjectStoreCoordinatorPool;
 import er.extensions.eof.ERXObjectStoreCoordinatorSynchronizer;
